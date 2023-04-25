1b6b397f5fecb98c9324713bbbe2c40201a53168
Egan Bisma <brickkace@gmail.com>
2023-04-26 00:59:20 +0700

    use cloudfront

--- src/components/DemoSteps.jsx
+++ src/components/DemoSteps.jsx
@@ -1,26 +1,29 @@
 import { useState, useRef, useEffect } from 'react'
 import { Container } from '@/components/Container'
 
+const CLOUDFRONT_URL =
+  'https://promptsandbox-landing.s3.us-west-1.amazonaws.com'
+
 const tabs = [
   {
     name: 'Step 1',
-    videoSource: './step1',
-    videoPoster: './step1-poster.png',
+    videoSource: 'step1',
+    videoPoster: 'step1-poster.webp',
   },
   {
     name: 'Step 2',
-    videoSource: './step2',
-    videoPoster: './step2-poster.png',
+    videoSource: 'step2',
+    videoPoster: 'step2-poster.webp',
   },
   {
     name: 'Step 3',
-    videoSource: './step3',
-    videoPoster: './step3-poster.png',
+    videoSource: 'step3',
+    videoPoster: 'step3-poster.webp',
   },
   {
     name: 'The App',
-    videoSource: './the-app',
-    videoPoster: './the-app-poster.png',
+    videoSource: 'the-app',
+    videoPoster: 'the-app-poster.webp',
   },
 ]
 
@@ -153,42 +156,62 @@ export function DemoSteps() {
         <div className="mx-auto inline-block bg-slate-50 sm:mt-5 sm:pt-4">
           <div className="-m-2 inline-block rounded-xl bg-slate-900/5 p-2 ring-1 ring-inset ring-slate-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
             {selectedTab.name == 'The App' ? (
-              <video
-                controls
-                onClick={handleVideoClick}
-                preload="none"
-                playsInline
-                poster={selectedTab.videoPoster}
-                key={`${selectedTab.videoSource}.webm`}
-                className=" mx-auto max-h-[40rem] rounded-md shadow-2xl ring-1 ring-slate-900/10"
-              >
-                <source
-                  src={`${selectedTab.videoSource}.webm`}
-                  type="video/webm"
-                />
-                <source
-                  src={`${selectedTab.videoSource}.mp4`}
-                  type="video/mp4"
-                />
-              </video>
+              <>
+                <video
+                  controls
+                  onClick={handleVideoClick}
+                  preload="none"
+                  playsInline
+                  poster={`${CLOUDFRONT_URL}/${selectedTab.videoPoster}`}
+                  key={`${CLOUDFRONT_URL}/${selectedTab.videoSource}.webm`}
+                  className="mx-auto hidden h-[40rem] rounded-md shadow-2xl ring-1 ring-slate-900/10 sm:block"
+                >
+                  <source
+                    src={`${CLOUDFRONT_URL}/${selectedTab.videoSource}.webm`}
+                    type="video/webm"
+                  />
+                  <source
+                    src={`${CLOUDFRONT_URL}/${selectedTab.videoSource}.mp4`}
+                    type="video/mp4"
+                  />
+                </video>
+                <video
+                  controls
+                  onClick={handleVideoClick}
+                  playsInline
+                  preload="none"
+                  poster={`${CLOUDFRONT_URL}/${selectedTab.videoPoster}`}
+                  key={`${selectedTab.videoSource}.webm2`}
+                  className="mx-auto h-[40rem] rounded-md shadow-2xl ring-1 ring-slate-900/10 sm:hidden"
+                >
+                  <source
+                    src={`${CLOUDFRONT_URL}/${selectedTab.videoSource}.webm`}
+                    type="video/webm"
+                  />
+                  <source
+                    src={`${CLOUDFRONT_URL}/${selectedTab.videoSource}.mp4`}
+                    type="video/mp4"
+                  />
+                </video>
+              </>
             ) : (
               <video
-                key={`${selectedTab.videoSource}.webm`}
+                key={`${CLOUDFRONT_URL}/${selectedTab.videoSource}.webm`}
                 // controls
                 muted={true}
                 autoPlay
                 loop
                 playsInline
                 preload="none"
-                poster={selectedTab.videoPoster}
-                className="lazy rounded-m  mx-auto max-h-[40rem] shadow-2xl ring-1 ring-slate-900/10"
+                poster={`${CLOUDFRONT_URL}/${selectedTab.videoPoster}`}
+                className="lazy mx-auto  h-[40rem] rounded-md shadow-2xl ring-1 ring-slate-900/10"
               >
                 <source
-                  src={`${selectedTab.videoSource}.webm`}
+                  src={`${CLOUDFRONT_URL}/${selectedTab.videoSource}.webm`}
                   type="video/webm"
                 />
                 <source
-                  src={`${selectedTab.videoSource}.mp4`}
+                  src={`${CLOUDFRONT_URL}/${selectedTab.videoSource}.mp4`}
                   type="video/mp4"
                 />
               </video>
